c9f2dc488fa047356907a98f28e00aa23604f9ff
Sajid Ali <81649787+sajidm257@users.noreply.github.com>
2022-11-15 08:53:33 +0500

    delete functionality created

---
+++ admin/delete-user.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+$userid = $_GET['id'];
+
+$sql = "DELETE FROM user WHERE user_id = {$userid}";
+
+if(mysqli_query($conn, $sql)){
+    header("Location:{$hostname}/admin/users.php");
+}else{
+    echo "Cannot Delete User";
+}
+
+
+mysqli_close($conn);
+?>
