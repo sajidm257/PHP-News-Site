8727127ab3295da83b4df371e90a69921d477b73
Sajid Ali <81649787+sajidm257@users.noreply.github.com>
2022-11-30 17:10:21 +0500

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -4,10 +4,19 @@
 
 <div >
     <h1 align="center">All Users</h1>
+    <a href="add-user.php">Add New User</a>
 <?php
 include 'config.php';
+$limit=3;
+// $page = $_GET['page'];
+if(isset($_GET['page'])){
+    $page = $_GET['page'];
+}else{
+    $page = 1;
+};
+$offset = ($page - 1) * $limit;
 
-$sql = "SELECT * FROM user ORDER BY user_id DESC";
+$sql = "SELECT * FROM users ORDER BY user_id DESC LIMIT {$offset}, {$limit}";
 
 $result = mysqli_query($conn, $sql);
 
@@ -46,11 +55,38 @@ if (mysqli_num_rows($result) > 0) {
         <?php } ?>
         </tbody>
     </table>
-    <?php } ?>
+    <?php } 
+    
+    $sql1 = "SELECT * FROM users";
+    $result1 = mysqli_query($conn,$sql1) or die("Query Failed");
+    
+    if(mysqli_num_rows($result)>0){
+        $total_records=mysqli_num_rows($result1);
+        
+        $total_page = ceil($total_records / $limit);
+        
+        echo '<ul>';
+        if($page>1){
+        echo'<li><a href="users.php?page='.($page-1).'">Prev</a></li>';
+        };
+        
+        for($i=1; $i<=$total_page; $i++){
+            if($i==$page){
+                $active="active";
+            }else{$active="";}
+            echo'<li class="'.$active.'"> <a href="users.php?page='.$i.'">'.$i.'</a></li>';
+        }
+        if($total_page > $page){
+        echo'<li><a href="users.php?page='.($page+1).'">Next</a></li>';
+        };
+        
+        echo'</ul>';
+    }
+    ?>
 
-    <ul>
-        <li><a>1</a></li>
-        <li><a>2</a></li>
-        <li><a>3</a></li>
+    <!--<ul>-->
+    <!--    <li><a href="#">1</a></li>-->
+    <!--    <li><a href="#">2</a></li>-->
+    <!--    <li><a href="#">3</a></li>-->
     </ul>
 </div>
